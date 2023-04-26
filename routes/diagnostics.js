7e12f8df80de9d40a812d6b11595695de9bfe2b7
Cassie T <cassandray9@gmail.com>
2023-04-26 14:02:14 -0600

    diagnostics.js completed

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,31 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile('./db/diagnostics.json').then((data) => res.json(JSON.parse(data)))
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+  const { isValid , errors } = req.body;
+
+  if (isValid && errors) {
+    const payLoad = {
+      time: Date.now(),
+      error_id: uuidv4(),
+      errors,
+    }
+
+    if(!isValid) {
+      readAndAppend(payLoad, './db/diagnostics.json');
+      res.json(`Diagnostic Info added`);
+    } else {
+      res.json({ 
+        message: 'Error in posting tip',
+        error_id: payLoad.error_id,
+      });
+    }
+  }
 });
 
 module.exports = diagnostics;
